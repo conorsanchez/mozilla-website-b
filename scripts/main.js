c56f99d97088eb9f67d65ba1f121000a21e4a045
CONOR SANCHEZ <CONOR.SANCHEZ@GMAIL.COM>
2018-01-10 21:12:15 -0500

    add everything

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,13 @@
  function sayOuch() {
  	alert('Ouch! Stop poking me!');
  }
+
+ function sayWoof () {
+ 	alert ('Woof!'):
+ }
  
  // Use a CSS selector to identify an element
- var foxImage = document.querySelector('img');
+ var foxImage = document.querySelector('#foxpic');
  
  // Assign the function to the onclick event on that element
  foxImage.onclick = sayOuch;
